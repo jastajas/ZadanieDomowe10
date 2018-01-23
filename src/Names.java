b645143443040669fea6656fa4614280171fdd6a
jackS <jastajas@gamil.com>
2018-01-23 18:37:03 +0100

    Klasa Names (main) - ver. 1 tablica z imionami

---
+++ src/Names.java
@@ -0,0 +1,12 @@
+public class Names {
+    public static void main (String[] args){
+        String[] names = new String[3];
+        names[0] = "Kasia";
+        names[1] = "Asia";
+        names[2] = "Basia";
+
+        for(String n : names) {
+            System.out.println(n);
+        }
+    }
+}
